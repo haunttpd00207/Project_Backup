a8fa9c7f969a9daccd7ccff6236c5e0a829d181f
haunttpd00207 <haunttpd00207@gmail.com>
2019-10-30 13:43:51 +0700

    RUBY_DEV-190_add_menus:OrderComparer(GY) v1.0対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :order_comparers, path: Rails.env.production? ? "" : "order-comparer", only: [] do
+    get "/index", action: :index
+  end
 end
