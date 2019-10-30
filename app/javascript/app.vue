01d89fee880948961786918c51e3adcf54e024c4
haunttpd00207 <haunttpd00207@gmail.com>
2019-10-30 13:49:20 +0700

    RUBY_DEV-190_add_form_import:OrderComparer(GY) v1.0対応

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,135 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+<div>
+  <h4 class="page-title custom-title">{{ $t("order_comparer.title") }}</h4>
+  <div class="x_panel">
+    <div class="x_content">
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12" style="font-size: 18px">{{ $t("order_comparer.label_fmt") }}</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12">
+            <div class="form-table">
+              <div class="form-row">
+                <div class="form-cell cell-label">
+                  <span style="padding-left: 50px">{{ $t("order_comparer.desc_url") }}</span>
+                </div>
+                <div class="form-cell.cell-input">
+                  <input type="text" class="form-control" :placeholder="$t('order_comparer.hide_fmt')">
+                  <span class="input-group-btn">
+                    <b-button variant="primary" class="custom-button">{{ $t('order_comparer.import') }}</b-button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12" style="font-size: 18px">{{ $t("order_comparer.label_google") }}</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12">
+            <div class="form-table">
+              <div class="form-row">
+                <div class="form-cell cell-label">
+                  <span style="padding-left: 50px">{{ $t("order_comparer.desc_url") }}</span>
+                </div>
+                <div class="form-cell.cell-input">
+                  <input type="text" class="form-control" :placeholder="$t('order_comparer.hide_google')">
+                  <span class="input-group-btn">
+                    <b-button variant="primary" class="custom-button">{{ $t('order_comparer.import') }}</b-button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12" style="font-size: 18px">{{ $t("order_comparer.label_yahoo") }}</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group">
+          <div class="col-md-12">
+            <div class="form-table">
+              <div class="form-row">
+                <div class="form-cell cell-label">
+                  <span style="padding-left: 50px">{{ $t("order_comparer.desc_url") }}</span>
+                </div>
+                <div class="form-cell.cell-input">
+                  <input type="text" class="form-control" :placeholder="$t('order_comparer.hide_yahoo')">
+                  <span class="input-group-btn">
+                    <b-button variant="primary" class="custom-button">{{ $t('order_comparer.import') }}</b-button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <button type="button" class="btn btn-info">{{ $t("order_comparer.submit") }}</button>
+          <span style="padding-left: 20px">{{ $t("order_comparer.desc_submit") }}</span>
+        </div>
+      </div>
+    </div>
   </div>
+  
+  <div class="x_panel no-border no-background" >
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-table">
+          <div class="form-row">
+            <div class="form-cell cell-label">
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_title") }}</span>
+              <br>
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_content_1") }}</span>
+              <br>
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_content_2") }}</span>
+              <br>
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_content_3") }}</span>
+              <br>
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_content_4") }}</span>
+              <br>
+              <span style="padding-left: 50px">{{ $t("order_comparer.note_content_5") }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
-
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
 </script>
-
 <style scoped>
 p {
-  font-size: 2em;
-  text-align: center;
+font-size: 2em;
+text-align: center;
+}
+.hidden {
+display: none;
+}
+.custom-button {
+border-top-left-radius: 0;
+border-bottom-left-radius: 0;
+}
+.form-control:disabled, .form-control[readonly] {
+background-color: white;
 }
 </style>
---
+++ app/javascript/locales/ja.js
@@ -0,0 +1,25 @@
+const messages = {
+  ja: {
+  	order_comparer: {
+  		title: "Order Comparer for サーチ・ディスプレイ・動画",
+  		label_fmt: "①指示書（公式依頼FMT）",
+  		label_google: "②Google掲載内容",
+  		label_yahoo: "③Yahoo掲載内容",
+  		hide_fmt: "指示書（Excel）を選択してください",
+  		hide_google: "Googleの掲載内容（csv）を選択してください",
+  		hide_yahoo: "Yahooの掲載内容（csv）を選択してください",
+  		desc_url: "ファイル選択: ",
+  		submit: "チェック実行",
+  		desc_submit: "※結果は①にシート追加された形でダウンロードされます",	
+  		note_title: "＜注意事項＞",
+  		note_content_1: "・①指示書ファイルでのシート削除、シート名の変更、列の追加は禁止です",
+  		note_content_2: "・②Google Adwords Editorからアカウント全体をエクスポートしたファイルを選択してください",
+  		note_content_3: "・③サーチの場合はYahooキャンペーンエディターから、ディスプレイ・動画の場合はYDNキャンペーンエディターから",
+  		note_content_4: "エクスポート＞すべてのコンポーネントを出力したファイルを選択してください",
+  		note_content_5: "・サーチ・ディスプレイ・動画は①指示書の内容により自動で判定されます",
+  		import: "参照..."
+  	}
+  }
+}
+
+export default messages
